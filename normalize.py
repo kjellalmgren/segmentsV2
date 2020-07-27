3a2d7d9feeed87a1a7e95df4b01fcb56e46a2de3
kjellalmgren <kjell.almgren@tetracon.se>
2020-07-27 22:47:14 +0200

    New normalization in dataframe

--- normalize.py
+++ normalize.py
@@ -8,15 +8,17 @@ columns = ['indicies', 'Region', 'Office', 'Revenue', 'Region_normalized', 'Offi
 df = pd.read_csv('datasets/segment_training_v5.csv')
 
 # add new column to the dataframe
+#
 df.insert(3, "Region_normalized", 0.0)
 df.insert(4, "Office_normalized", 0.0)
 df.insert(5, "Revenue_normalized", 0.0)
 #
-print(df.columns)
-print(type('Revenue_normalized'))
+#print(df.columns)
+#print(type('Revenue_normalized'))
 #
 # copy Revenue to Revenue_normalized, Office to Office_normalized and Region to Region_normalized
 #
+print("Creating column to be normalized")
 for i in df.index:
     df.at[i, 'Region_normalized'] = df.at[i, 'Region']
     df.at[i, 'Office_normalized'] = df.at[i, 'Office']
@@ -34,7 +36,7 @@ print("y: ", y)
 #
 # Print first and last five rows in the dataset to be able to se the new columns
 #
-print(x.head(5))
+#print(x.head(5))
 #
 # Split data into train and test
 #
@@ -46,15 +48,24 @@ print(x_train.head(5))
 #
 # Select numerical columns which needs to be normalized
 #
-#train_norm = x_train[x_train.columns[0:6]]
-# test_norm = x_test[x_test.columns[0:6]]
+train_norm = x_train[x_train.columns[3:]]
+test_norm = x_test[x_test.columns[3:]]
 #
 # Normalize Training Data 
 #
-#std_scale = preprocessing.StandardScaler().fit(train_norm)
-#x_train_norm = std_scale.transform(train_norm)
+std_scale = preprocessing.StandardScaler().fit(train_norm)
+x_train_norm = std_scale.transform(train_norm)
 #
 # Converting numpy array to dataframe and update x_train
 #
-#training_norm_col = pd.DataFrame(x_train_norm, index=train_norm.index, columns=train_norm.columns) 
-#x_train.update(training_norm_col)
+training_norm_col = pd.DataFrame(x_train_norm, index=train_norm.index, columns=train_norm.columns) 
+x_train.update(training_norm_col)
+#
+x_test_norm = std_scale.transform(test_norm)
+testing_norm_col = pd.DataFrame(x_test_norm, index=test_norm.index, columns=test_norm.columns) 
+x_test.update(testing_norm_col)
+#
+# Show result
+#
+print(x_train.head(5))
+print(x_test.head(5))
--- predict_segmentv5.py
+++ predict_segmentv5.py
@@ -69,6 +69,8 @@ with tf.device("/device:gpu:0"):
         class_id = pred_dict['class_ids'][0]
         probability = pred_dict['probabilities'][class_id]
         print(pred_dict)
-        print('Prediction is "{}" ({:.1f}%)'.format(
-            SPECIES[class_id], 100 * probability))
+        #print('Prediction is "{}" ({:.1f}%)'.format(
+        #    SPECIES[class_id], 100 * probability))
+        print('Prediction is "{}" ({:f}%)'.format(
+            SPECIES[class_id], probability))
 #
