22aed5f2669feab1f3f998ef4780806beaef9e2c
kjellalmgren <kjell.almgren@tetracon.se>
2020-07-28 20:02:00 +0200

    Changed normalization

--- normalize.py
+++ normalize.py
@@ -3,12 +3,14 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
+print("Reading data datasets/segment_training_v5.csv...")
 columns = ['indicies', 'Region', 'Office', 'Revenue', 'Region_normalized', 'Office_normalized', 'Revenue_normalized']
 # columns = ['indicies', 'Region_normalized', 'Office_normalized', 'Revenue_normalized']
 df = pd.read_csv('datasets/segment_training_v5.csv')
 
 # add new column to the dataframe
 #
+print("Creating column to be used for normalizing...")
 df.insert(3, "Region_normalized", 0.0)
 df.insert(4, "Office_normalized", 0.0)
 df.insert(5, "Revenue_normalized", 0.0)
@@ -18,7 +20,7 @@ df.insert(5, "Revenue_normalized", 0.0)
 #
 # copy Revenue to Revenue_normalized, Office to Office_normalized and Region to Region_normalized
 #
-print("Creating column to be normalized")
+print("Adding values to normalized columns...")
 for i in df.index:
     df.at[i, 'Region_normalized'] = df.at[i, 'Region']
     df.at[i, 'Office_normalized'] = df.at[i, 'Office']
@@ -38,26 +40,27 @@ print("y: ", y)
 #
 #print(x.head(5))
 #
-# Split data into train and test
+# Split data into train and test dataset, just 75%
 #
 x_train, x_test, y_train, y_test = train_test_split(
                                     x, y,
-                                    train_size=0.7,
+                                    train_size=0.75,
                                     random_state=90)
 print(x_train.head(5))
 #
 # Select numerical columns which needs to be normalized
 #
+print("Start normalizing columns for Region, office and revenue...")
 train_norm = x_train[x_train.columns[3:]]
 test_norm = x_test[x_test.columns[3:]]
 #
 # Normalize Training Data 
 #
 std_scale = preprocessing.StandardScaler().fit(train_norm)
-x_train_norm = std_scale.transform(train_norm)
 #
 # Converting numpy array to dataframe and update x_train
 #
+x_train_norm = std_scale.transform(train_norm)
 training_norm_col = pd.DataFrame(x_train_norm, index=train_norm.index, columns=train_norm.columns) 
 x_train.update(training_norm_col)
 #
@@ -67,5 +70,21 @@ x_test.update(testing_norm_col)
 #
 # Show result
 #
-print(x_train.head(5))
-print(x_test.head(5))
+print(x_train.tail(15))
+print(x_test.tail(15))
+# 
+inverses = std_scale.inverse_transform(training_norm_col)
+
+j = 1
+for inverse in inverses:
+    #print(inverse)
+    #print("Values: {0:6f} - {1:3.1f} - {2:3.1f} - {3:10.1f}".format(j, inverse[0], inverse[1], inverse[2]))
+    j = j + 1
+#
+print("Number of records in traning dataset: ", len(x))
+print("Number of records in test dataset: ", len(y))
+print("Inverse: (75%) ", len(x_train_norm))
+print("Normalized: (75) ", len(inverses))
+print("Preprocessing done...")
+
+#print('Prediction is "{}" ({:f}%)'.format(SPECIES[class_id], probability))
---
+++ normalize1.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from pandas import read_csv
+from sklearn import preprocessing
+
+# from sklearn.model_selection import train_test_split
+# import tensorflow as tf
+
+# data = [10.0, 11.0, 656000.0]
+columns = ['Region', 'Office', 'Revenue', 'Segment', 'Revenue_normalized']
+
+series = read_csv('datasets/segment_training_v5.csv', header=0, index_col=0)
+
+#series = series.pop('Segment')
+values = series.values
+print(values)
+print(series.head())
+values = values.reshape((len(values), 3))
+print(type(values))
+scaler = preprocessing.StandardScaler()
+scaler = scaler.fit(values)
+normalized = scaler.transform(values)
+print(type(normalized))
+# np.sort(normalized,axis=0, kind='quicksort')
+for i in range(len(normalized)):
+    print("X: ", i,normalized[i])
+# print(normalized)
+inverse = scaler.inverse_transform(normalized)
+print(type(inverse))
+#
+for i in range(len(inverse)):
+    #print("{0:6.1f}".format(inverse[i]))
+    print("Y: {} - {}".format(i, inverse[i]))
+    #print("Y: {:3.0f} - {:6.2f}".format(i, inverse[i]))
+#
+print("Antal: ", len(values))
+print("Inverse: ", len(inverse))
+print("Normalized: ", len(normalized))
+#print(inverse)
+#training_norm_col = pd.DataFrame(x) 
+#x.update(training_norm_col)
+#print("Values: {0:3.1f} - {1:3.1f} - {2:10.1f} - {3:10.1f}".format(inverse[0], inverse[1], inverse[2], inverse[3]))
+#print("Values: {0:d}".format(inverse[0]))
+
+#
+# Converting numpy array to dataframe and update x_train
+#
+# x_train_norm = std_scale.transform(data)
+# training_norm_col = pd.DataFrame(x_train_norm, index=train_norm.index, columns=train_norm.columns) 
+# x_train.update(training_norm_col)
