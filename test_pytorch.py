b0c63edd39598988b445dc3b09fb8bc95f26b69e
kjellalmgren <kjell.almgren@tetracon.se>
2020-09-14 22:02:58 +0200

    pytorch changes

--- test_pytorch.py
+++ test_pytorch.py
@@ -1,15 +1,36 @@
 from __future__ import print_function
+from sklearn import preprocessing
 import torch
 import torch.utils.data
 import torch.nn as nn
+import numpy as np
 import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+#
+#
+# determine the supported device
+def get_device():
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device('cpu') # don't have GPU 
+    return device
+#
+# convert a df to tensor to be used in pytorch
+def df_to_tensor(df):
+    device = get_device()
+    return torch.from_numpy(df.values).float().to(device)
+#
+# here we go...
+#
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+#
+np.set_printoptions(suppress=True)
+#
 print("Cuda cores availablity: ", torch.cuda.is_available())
 print(device)
 x = torch.rand(5, 3).cuda()
@@ -17,14 +38,27 @@ print(x)
 #
 # Set default to whatever device is available
 #
-
-
 #Read the data from csv file
+print("Reading data datasets/segment_training_v5.csv...")
+columns = ['indicies', 'Region', 'Office', 'Revenue', 'Region_normalized', 'Office_normalized', 'Revenue_normalized']
 df = pd.read_csv('datasets/segment_training_v5.csv')
 print(df.shape)
 print(df.describe())
+#
+print("Creating column to be used for normalizing...")
+df.insert(3, "Region_normalized", 0.0)
+df.insert(4, "Office_normalized", 0.0)
+df.insert(5, "Revenue_normalized", 0.0)
+#
+# copy Revenue to Revenue_normalized, Office to Office_normalized and Region to Region_normalized
+#
+print("Adding values to normalized columns...")
+for i in df.index:
+    df.at[i, 'Region_normalized'] = df.at[i, 'Region']
+    df.at[i, 'Office_normalized'] = df.at[i, 'Office']
+    df.at[i, 'Revenue_normalized'] = df.at[i, 'Revenue']
+#
 # Select predictors
-
 target_column = ['Segment']
 predictors = list(set(list(df.columns))-set(target_column))
 
@@ -33,8 +67,56 @@ print("prediction columns: ", predictors)
 
 x = df[predictors].values
 y = df[target_column].values
+
+x1 = df[df.columns[:6]]
+print(x1.head(5))
+#
+# y is the eight's column, label for each row in the dataset (segment)
+#
+y1 = df.Segment
+print("y1: ", y1)
+#
 #Split data into train and test 
-x_train, x_test, y_train, y_test = train_test_split(x, y , train_size = 0.7, random_state =  90)
+x_train, x_test, y_train, y_test = train_test_split(x1, y1, train_size = 0.7, random_state =  90)
+#
+#
+# Select numerical columns which needs to be normalized
+#
+print("Start normalizing columns for Region, office and revenue...")
+train_norm = x_train[x_train.columns[3:]]
+test_norm = x_test[x_test.columns[3:]]
+#
+# Normalize Training Data 
+#
+std_scale = preprocessing.StandardScaler().fit(train_norm)
+#
+# Converting numpy array to dataframe and update x_train
+#
+x_train_norm = std_scale.transform(train_norm)
+training_norm_col = pd.DataFrame(x_train_norm, index=train_norm.index, columns=train_norm.columns) 
+x_train.update(training_norm_col)
+#
+x_test_norm = std_scale.transform(test_norm)
+testing_norm_col = pd.DataFrame(x_test_norm, index=test_norm.index, columns=test_norm.columns) 
+x_test.update(testing_norm_col)
+#
+# Show result
+#
+print(x_train.tail(15))
+print(x_test.tail(15))
+# 
+inverses = std_scale.inverse_transform(training_norm_col)
+j = 1
+for inverse in inverses:
+    #print(inverse)
+    #print("Values: {0:6f} - {1:3.1f} - {2:3.1f} - {3:10.1f}".format(j, inverse[0], inverse[1], inverse[2]))
+    j = j + 1
+#
+print("Number of records in traning dataset: ", len(x1))
+print("Number of records in test dataset: ", len(y1))
+print("Inverse: (75%) ", len(x_train_norm))
+print("Normalized: (75) ", len(inverses))
+print("Preprocessing done...")
 #
 print(x_train.shape); print(x_test.shape)
 
@@ -66,15 +148,28 @@ print(model)
 #
 # block
 #
-x_train = torch.from_numpy(x_train)
-x_train = x_train.cuda()
-y_train = torch.from_numpy(y_train).view(-1,1)
-y_train = y_train.cuda()
-
-x_test = torch.from_numpy(x_test)
-x_test = x_test.cuda()
-y_test = torch.from_numpy(y_test).view(-1,1)
-y_test = y_test.cuda()
+# denna del har förändrats, vi behöver se över x_train, y_train
+# vi ska prova med att plocka bort region, office samt revenue och bara behålla normalized columns
+# torch.Tensor(np.array(df))
+# x_train = torch.from_numpy(torch.Tensor(np.array(x_train))).cuda()
+x_train = df.select_dtypes(include=float).to_numpy()
+# x_train = x_train.cuda()
+#y_train = torch.from_numpy(df_to_tensor(y_train)).view(-1,1)
+##y_train = torch.from_numpy(torch.Tensor(np.array(y_train))).view(-1,1)
+y_train = df.select_dtypes(include=float).to_numpy()
+#y_train = y_train.view(-1,1)
+#y_train = y_train.cuda()
+np.set_printoptions(suppress=True)
+print("x_train...")
+print(x_train[:10])
+print("----------")
+#
+#x_test = torch.from_numpy(df_to_tensor(x_test))
+x_test = df.select_dtypes(include=float).to_numpy()
+#x_test = x_test.cuda()
+#y_test = torch.from_numpy(df_to_tensor(y_test)).view(-1,1)
+y_test = df.select_dtypes(include=float).to_numpy()
+#y_test = y_test.cuda()
 #
 # Block
 #
@@ -92,7 +187,7 @@ optimizer = optim.SGD(model.parameters(), lr=0.001, weight_decay= 1e-6, momentum
 
 # lines 1 to 6
 #epochs = 2000
-epochs = 100
+epochs = 10     # default 100
 epoch_list = []
 train_loss_list = []
 val_loss_list = []
